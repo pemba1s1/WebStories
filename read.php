f22254db360d7787edc4176b54db9c99932e38b5
pemba1s1 <norsangpemba@gmail.com>
2021-10-17 14:28:06 +0545

    next and prev chap redirect when chap not found

--- read.php
+++ read.php
@@ -5,7 +5,11 @@ $novel_name=$_GET['novel_name'];
 $chapter_no=$_GET['chapter_no'];
 $sql="SELECT * from `$novel_name` where chapter_no='$chapter_no'";
 $result=mysqli_query($conn,$sql);
-
+$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+if($count==0){
+    header('Location:about.php?novel_name='.$novel_name);
+}
 if($result){
     foreach($result as $key=>$novel){
         $content=$novel['content'];
@@ -24,7 +28,7 @@ else{
             <a href="about.php?novel_name=<?php echo $novel_name;?>">
                 <?php
                     echo $novel_name;
-                ?>
+                ?>  
             </a>
         </div>
         <div class="next">
@@ -34,11 +38,7 @@ else{
     <div class="read">
         <div class="read-title">
             <?php 
-                if(!$chapter_title){
-                    header('Location:about.php?novel_name='.$novel_name);
-                }
-                else
-                    echo "Chapter ". $chapter_no ." ".$chapter_title;
+                echo "Chapter ". $chapter_no ." ".$chapter_title;
             ?>
         </div>
         <div class="read-content">
